c798cfeee313f0806cdb74f1470dc7edee95fb46
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-21 10:11:22 -0300

    att

--- AEDS2-C&JAVA/tps/tp1/exercicio2.c
+++ AEDS2-C&JAVA/tps/tp1/exercicio2.c
@@ -1,7 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main () {
-    
+int palindromo (char str[]) { // método para verificar se a string é um palíndromo
+    int resp = 0; //declaração do retorno inteiro do método
+    if (str[0] == '\0')
+
+    return resp; // retorno do método
+}
+
+int main () { // main do programa
+    char str[100]; scanf(" %[^\n]", str); //declaração e leitura da string
+    while (!(str[0] == 'F' && str[1] == 'I' && str[2] == 'M' && str[3] == '\0')) { // loop para ler string e verificar se essa é um palíndromo enquanto a string seja diferente de "FIM"
+        if (palindromo(str) == 1) printf("SIM\n"); // se for palíndromo, imprime "SIM"
+        else printf("NAO\n"); // se não, imprime "NAO"
+        scanf(" %[^\n]", str); // leitura da próxima string
+    }
     return 0;
 }
