89dda63b5718be26ad0e2285fe477bbce5a99001
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-21 12:51:37 -0300

    att

--- AEDS2-C&JAVA/tps/tp2/exercicio3.java
+++ AEDS2-C&JAVA/tps/tp2/exercicio3.java
@@ -1,9 +1,14 @@
-import java.util.Scanner;
-
 public class exercicio3 {
+    public static String inversao (String str) {
+        String nova = "";
+        for (int i = str.length() - 1; i >= 0; i--) nova += str.charAt(i);
+        return nova;
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
+        String str = MyIO.readLine();
+        while (!str.equals("FIM")) {
+            System.out.println(inversao(str));
+            str = MyIO.readLine();
+        }
     }
 }
