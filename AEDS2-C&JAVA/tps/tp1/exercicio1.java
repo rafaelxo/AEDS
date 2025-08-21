98dd0f50b88d0454bfa43cd55a24a80bdac09c77
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-21 10:02:22 -0300

    att

--- AEDS2-C&JAVA/tps/tp1/exercicio1.java
+++ AEDS2-C&JAVA/tps/tp1/exercicio1.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
 
 public class exercicio1 {
-    
+    public static boolean palindromo (String str) { // método para verificar se a string é palíndromo
+        boolean resp = false; // declaração do retorno booleano do método
+        int i = 0, j = str.length() - 1; // declaração dos índices i e j (primeiro e último caracter da string)
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                resp = false; // se os caracteres forem diferentes, a string não é palíndromo
+                i = j; // atribuição de i igual à j para sair do loop
+            }
+            else {
+                resp = true; // se os caracteres forem iguais, a string pode ser palíndromo
+                i++; j--; // incrementa i e decrementa j para verificar os próximos caracteres
+            }
+        }
+        return resp; // retorno do método
+    }
+    public static void main (String[] args) { // main do programa
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine(); // declaração e leitura da string
+        while (!str.equals("FIM")) { // loop para ler strings e verifiar se essa é um palíndromo enquanto a string seja diferente de "FIM"
+            if (palindromo(str)) System.out.println("SIM"); // se for palíndromo, imprime "SIM"
+            else System.out.println("NAO"); //se não, imprime "NAO"
+            str = sc.nextLine(); // leitura da próxima string
+        }
+        sc.close();
+    }
 }
