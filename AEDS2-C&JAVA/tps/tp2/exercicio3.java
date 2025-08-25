b9087f1f22cbb2965d764cab2f2dbb2eb8d6e124
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-25 16:34:06 -0300

    att

--- AEDS2-C&JAVA/tps/tp2/exercicio3.java
+++ AEDS2-C&JAVA/tps/tp2/exercicio3.java
@@ -1,14 +1,14 @@
 public class exercicio3 {
-    public static String inversao (String str) {
-        String nova = "";
-        for (int i = str.length() - 1; i >= 0; i--) nova += str.charAt(i);
-        return nova;
+    public static String inversao (String str) { // método para realizar a inversao de uma string
+        String nova = ""; // declaração da string invertida como vazia
+        for (int i = str.length() - 1; i >= 0; i--) nova += str.charAt(i); // atribuição de cada caracter individualmente à cada posição da nova string
+        return nova; // retorno da nova string invertida
     }
-    public static void main (String[] args) {
-        String str = MyIO.readLine();
-        while (!str.equals("FIM")) {
-            System.out.println(inversao(str));
-            str = MyIO.readLine();
+    public static void main (String[] args) { // main do programa
+        String str = MyIO.readLine(); // declaração e leitura da string
+        while (!str.equals("FIM")) { // loop para ler strings e fazer sua inversão
+            System.out.println(inversao(str)); // saída da string invertida ao chamar o método
+            str = MyIO.readLine(); // leitura da próxima string
         }
     }
 }
