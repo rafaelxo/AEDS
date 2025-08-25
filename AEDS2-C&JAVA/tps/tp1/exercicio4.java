e242ffece4ebef3c644d4c89036a801f08d5b02c
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-25 11:07:12 -0300

    att

--- AEDS2-C&JAVA/tps/tp1/exercicio4.java
+++ AEDS2-C&JAVA/tps/tp1/exercicio4.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class exercicio4 {
     public static String alteracao (String str) { // método para realizar a alteração de caracteres
-        Random rand = new Random();
+        Random rand = new Random(); rand.setSeed(4); // predefine 4 como a seed do gerador para padronizar o resultado
         char c1 = (char)(rand.nextInt(26) + 'a'); char c2 = (char)(rand.nextInt(26) + 'a'); // gera dois caracteres aleatórios entre 'a' e 'z'
         String nova = ""; // declaração da string que armazenará a nova string com os caracteres alterados
         for (int i = 0; i < str.length(); i++) { // loop para percorrer cada caractere da string
