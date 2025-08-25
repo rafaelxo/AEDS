2b222318b1abacbd11c72ab79de1ba9b7995e2c7
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-25 16:54:15 -0300

    att

--- AEDS2-C&JAVA/tps/tp2/exercicio4.c
+++ AEDS2-C&JAVA/tps/tp2/exercicio4.c
@@ -1,17 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int somaDigitos (int n) {
-    if (n < 10) return n;
-    else return n % 10 + somaDigitos(n / 10);
+int somaDigitos (int n) { // método recursivo para somar cada caracter do número inteiro
+    if (n < 10) return n; // condição de parada, retornando o próprio número se for menor que 10
+    else return n % 10 + somaDigitos(n / 10); // soma do resto do número dividido por 10 e chamada recursiva do número dividido por 10
 }
 
-int main () {
-    int n;
-    scanf("%d", &n);
-    while (n != -1) {
-      printf("%d\n", somaDigitos(n));
-      scanf("%d", &n);
+int main () { // main do programa
+    int n; scanf("%d", &n); // declaração e leitura do número
+    while (n != -1) { // loop para ler números enquanto o número seja diferente de -1
+      printf("%d\n", somaDigitos(n)); // saída do resultado da soma dos caracteres
+      scanf("%d", &n); // leitura do próximo número
     }
     return 0;
 }
