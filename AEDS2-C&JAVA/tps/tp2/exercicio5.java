a99dff6be206f10cece74df0866b8a2430f7d232
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-25 17:02:04 -0300

    att

--- AEDS2-C&JAVA/tps/tp2/exercicio5.java
+++ AEDS2-C&JAVA/tps/tp2/exercicio5.java
@@ -1,13 +1,13 @@
 public class exercicio5 {
     public static boolean anagrama (String str1, String str2) {
         boolean resp = false;
-        
+
         return resp;
     }
     public static void main (String[] args) {
         String str1 = MyIO.readLine(); String str2 = MyIO.readLine();// declaração e leitura da string
         while (!str1.equals("FIM")) { // loop para ler duas strings e verifiar se essas são anagramas enquanto a string seja diferente de "FIM"
-            if (anagrama(str1, str2)) MyIO.println("SIM"); // se forem anagramas, imprime "SIM"
+            if (anagrama(str1, str2)) MyIO.println("SIM"); // chamada do método e validação de se forem anagramas, imprime "SIM"
             else MyIO.println("NAO"); // se não, imprime "NAO"
             str1 = MyIO.readLine(); str2 = MyIO.readLine();// leitura das próximas strings
         }
