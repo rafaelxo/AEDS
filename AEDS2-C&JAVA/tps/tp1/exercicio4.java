798ea282a4b91b2001ecc21d82750e5d7ca9b99d
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-26 11:02:53 -0300

    att

--- AEDS2-C&JAVA/tps/tp1/exercicio4.java
+++ AEDS2-C&JAVA/tps/tp1/exercicio4.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
 public class exercicio4 {
-    public static Random rand = new Random(4);
-    public static String alteracao (String str) { // método para realizar a alteração de caracteres
+    private static Random rand;
+    public exercicio4 () { rand = new Random(4); }
+    public String alteracao (String str) { // método para realizar a alteração de caracteres
         char c1 = (char)('a' + (Math.abs(rand.nextInt()) % 26)); char c2 = (char)('a' + (Math.abs(rand.nextInt()) % 26)); // gera dois caracteres aleatórios entre 'a' e 'z'
         String nova = ""; // declaração da string como char que armazenará a nova string com os caracteres alterados
         for (int i = 0; i < str.length(); i++) { // loop para percorrer cada caractere da string
@@ -12,9 +13,10 @@ public class exercicio4 {
         return nova; // retorno do método contendo a string
     }
     public static void main (String[] args) { // main do programa
+        exercicio4 e = new exercicio4();
         String str = MyIO.readLine(); // declaração e leitura da string
         while (!str.equals("FIM")) { // loop para ler strings e realizar a alteração enquanto a string seja diferente de "FIM"
-            System.out.println(alteracao(str)); // imprime o resultado da alteração ao chamar o método
+            MyIO.println(e.alteracao(str)); // imprime o resultado da alteração ao chamar o método
             str = MyIO.readLine(); // leitura da próxima string
         }
     }
