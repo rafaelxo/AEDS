db121cbb5f4ace65720f812fc50195c3b837849a
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-08-28 10:47:57 -0300

    att

--- AEDS2-C&JAVA/tps/tp2/exercicio1.java
+++ AEDS2-C&JAVA/tps/tp2/exercicio1.java
@@ -1,5 +1,9 @@
-public class exercicio1 {
-    public static void main (String[] args) {
+import java.util.*;
 
+public class exercicio1 {
+    public static Scanner sc = new Scanner (System.in);
+    public static void main (String[] args) {
+        int n = sc.nextInt();
+        
     }
 }
--- AEDS2-C&JAVA/tps/tp2/exercicio5.java
+++ AEDS2-C&JAVA/tps/tp2/exercicio5.java
@@ -23,7 +23,7 @@ public class exercicio5 {
         }
         return true;
     }
-    public static void main (String[] args) {
+    public static void main (String[] args) { // main do programa
         String str1 = MyIO.readLine(); String str2 = MyIO.readLine();// declaração e leitura da string
         while (!(str1.length() == 3 && str1.charAt(0) == 'F' && str1.charAt(1) == 'I' && str1.charAt(2) == 'M')) { // loop para ler duas strings e verifiar se essas são anagramas enquanto a string seja diferente de "FIM"
             if (anagrama(str1, str2)) MyIO.println("SIM"); // chamada do método e validação de se forem anagramas, imprime "SIM"
