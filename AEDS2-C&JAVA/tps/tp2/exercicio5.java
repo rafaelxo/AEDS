aba5863e263b07935da2812899c4dcefbf97c972
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-02 11:31:06 -0300

    att

--- AEDS2-C&JAVA/tps/tp2/exercicio5.java
+++ AEDS2-C&JAVA/tps/tp2/exercicio5.java
@@ -1,14 +1,23 @@
 public class exercicio5 {
+    public static void swap(char[] str, int i, int j) { // método para trocar dois caracteres de posição em um array
+        char temp = str[i]; // variável temporária para armazenar o valor do caractere na posição i
+        str[i] = str[j]; // atribui o valor do caractere na posição j ao caractere na posição i
+        str[j] = temp; // atribui o valor da variável temporária ao caractere na posição j
+    }
     public static void ordenar (char[] str) { // método para ordenar a string em ordem alfabética para assim facilitar a comparação
         for (int i = 0; i < str.length - 1; i++) { // loop até o penúltimo caractere
             for (int j = 0; j < str.length - i - 1; j++) { // loop até o penúltimo caractere menos o número de iterações já feitas
-                if (str[j] > str[j + 1]) { // se o caractere atual for maior que o próximo, troca os dois
-                    char temp = str[j]; // variável temporária para armazenar o valor do caractere atual
-                    str[j] = str[j + 1]; // atribui o valor do próximo caractere ao atual
-                    str[j + 1] = temp; // atribui o valor da variável temporária ao próximo caractere
-                }
+                if (str[j] > str[j + 1]) swap(str, j, j + 1); // se o caractere na posição j for maior que o próximo, chama o método para trocar os caracteres de posição
             }
         }
+    }
+    public static String minusculo (String str) { // método para converter todos os caracteres de uma string para minúsculo
+        String nova = ""; // declaração de uma nova string para armazenar a string em minúsculo
+        for (int i = 0; i < str.length(); i++) { // loop para percorrer todos os caracteres da string
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') nova += (char)(nova.charAt(i) + 32); // se o caractere for maiúsculo, converte o caractere para minúsculo
+            else nova += str.charAt(i); // se não, mantém o caractere
+        }
+        return nova; // retorna a nova string em minúsculo
     }
     public static boolean anagrama (String str1, String str2) { // método para verificar se duas strings são anagramas
         if (str1.length() != str2.length()) return false; // se o tamanho das strings for diferente, retorna falso
@@ -18,7 +27,8 @@ public class exercicio5 {
             aux2[i] = str2.charAt(i); // copia o caractere da string 2 para o array 2
         }
         ordenar(aux1); ordenar(aux2); // chama o método para ordenar os arrays
-        for (int i = 0; i < aux1.length; i++) { // loop para comparar os arrays ordenados até o tamanho do array 1
+        minusculo(str1); minusculo(str2); // chama o método para converter as strings para minúsculo
+        for (int i = 0; i < aux1.length; i++) { // loop para comparar os arrays atualizados até o tamanho do array 1
             if (aux1[i] != aux2[i]) return false; // se algum caractere for diferente, retorna falso
         }
         return true; // se todos os caracteres forem iguais, retorna verdadeiro
