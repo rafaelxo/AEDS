3d6db88c0535697ffd7b85cb7ca2acf13a5ec8a3
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2025-09-10 14:21:42 -0300

    att

--- AEDS2-C&JAVA/tps/tp3/exercicio3.java
+++ AEDS2-C&JAVA/tps/tp3/exercicio3.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class exercicio3 {
     public static Scanner sc = new Scanner (System.in);
+
+    public static boolean isFim(String str) { // método auxiliar para verificar se a string é igual a "FIM"
+        return (str.length() == 3 && str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(2) == 'M'); // retorna true ou false ao realizar a comparação
+    }
     public static int somaDigitos(String str, int i) { // método recursivo para somar cada caracter do número inteiro
         if (i >= str.length()) return 0; // condição de parada como '\0' qando a string acabar, retornando 0
         else return (str.charAt(i) - '0') + somaDigitos(str, i + 1); // soma o caracter convertido para inteiro e chama o método // recursivo com o índice incrementado
@@ -9,7 +13,7 @@ public class exercicio3 {
 
     public static void main (String[] args) {
         String num = sc.nextLine(); // declaração e leitura do número como string
-        while (!(num.length() == 3 && num.charAt(0) == 'F' && num.charAt(1) == 'I' && num.charAt(2) == 'M')) { // loop para ler números como string enquanto a string seja diferente de "FIM"
+        while (!isFim(num)) { // loop para ler números como string enquanto a string seja diferente de "FIM"
             System.out.println(somaDigitos(num, 0)); // saída do resultado da soma dos caracteres
             num = sc.nextLine(); // leitura do próximo número como string
         }
