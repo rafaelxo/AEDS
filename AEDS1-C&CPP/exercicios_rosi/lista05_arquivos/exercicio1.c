76a24ee20c23b09b740bdff2eacbc3b3c72fd8c5
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-02-10 14:01:39 +0000

    refatoração do repositório

--- AEDS1-C&CPP/exercicios_rosi/lista05_arquivos/exercicio1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main () {
-    FILE *arquivo = fopen("arq1.txt", "w");
-    for (int i = 1; i <= 10; i++) fprintf(arquivo, "%d\n", i);
-    fclose(arquivo);
-}
